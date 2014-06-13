6cd346119ed6605b0db9176db61fbd040e258a14
osyo-manga <manga.osyo@gmail.com>
2014-06-13 18:09:35 +0900

    first commit

---
+++ source/javascripts/script.js
@@ -0,0 +1,22 @@
+var StripeDemoCtrl = function ($scope) {
+
+	$scope.click = function (text){
+		console.log(text);
+	}
+
+	$scope.months = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
+	]
+};
+
